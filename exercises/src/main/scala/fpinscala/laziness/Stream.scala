aa0b696f268cc389dd56ab863a519dead19d3d89
Yifu Duan <yifu.duan@shopee.com>
2019-03-12 14:45:04 +0800

    [ch5]Add answers for Stream exercises

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -10,10 +10,10 @@ trait Stream[+A] {
       case _ => z
     }
 
-//  def foldLeft[B](z: => B)(f: (=> B, A) => B): B = this match {
-//    case Cons(h, t) => t().foldLeft(f(z, h))(f)
-//    case _ => z
-//  }
+  //  def foldLeft[B](z: => B)(f: (=> B, A) => B): B = this match {
+  //    case Cons(h, t) => t().foldLeft(f(z, h))(f)
+  //    case _ => z
+  //  }
 
   def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
@@ -51,7 +51,7 @@ trait Stream[+A] {
 
   // ex 5.3
   def takeWhile(p: A => Boolean): Stream[A] = this match {
-//    case Cons(h, t) if p(h) => cons(h(), t().takeWhile(p))
+    //    case Cons(h, t) if p(h) => cons(h(), t().takeWhile(p))
     case Cons(h, t) =>
       if (p(h())) cons(h(), t().takeWhile(p))
       else this
@@ -93,8 +93,6 @@ trait Stream[+A] {
     )
 
 
-
-
   // flat(this.map(f))
   def flatMap[B](f: A => Stream[B]): Stream[B] =
     foldRight(empty[B])((a, b) => f(a) append b)
@@ -106,11 +104,78 @@ trait Stream[+A] {
     case Cons(h, t) => h() :: t().toList
   }
 
+  // ex5.13
+  def map1[B](f: A => B): Stream[B] =
+    unfold(this) {
+      case Cons(h, t) => Some(f(h()), t())
+      case _ => None
+    }
+
+  def take1(n: Int): Stream[A] =
+    unfold((this, n)) {
+      case (Cons(h, t), m) if m > 0 => Some(h(), (t(), m - 1))
+      case _ => None
+    }
+
+
+  def takeWhile2(p: A => Boolean): Stream[A] =
+    unfold(this) {
+      case Cons(h, t) if p(h()) => Some(h(), t())
+      case _ => None
+    }
+
+  def With[B, C](bs: Stream[B])(f: (A, B) => C): Stream[C] =
+    unfold((this, bs)) {
+      case (Cons(l, ls), Cons(r, rs)) =>
+        Some(
+          f(l(), r()) -> (ls(), rs())
+        )
+      case _ => None
+    }
+
+  def All[B](that: Stream[B]): Stream[(Option[A], Option[B])] =
+    unfold((this, that)) {
+      case (Cons(l, ls), Cons(r, rs)) =>
+        Some(
+          (Some(l()), Some(r())) -> (ls(), rs())
+        )
+      case (Cons(l, ls), Empty) =>
+        Some(
+          (Some(l()), None) -> (ls(), empty)
+        )
+      case (Empty, Cons(r, rs)) =>
+        Some(
+          (None, Some(r())) -> (empty, rs())
+        )
+      case _ => None
+    }
+
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
-  def startsWith[B](s: Stream[B]): Boolean = ???
+  // 5.14
+  def startsWith[B](s: Stream[B]): Boolean =
+    this.All(s).takeWhile(_._2.nonEmpty) forAll {
+      case (l, r) => l == r
+    }
+
+  // 5.15
+  def tails: Stream[Stream[A]] =
+    unfold(this) {
+      case Empty => None
+      case s => Some(s -> s.drop(1))
+    } append empty
+
+  def hasSubsequence[A](s: Stream[A]): Boolean =
+    tails exists (_ startsWith s)
+
+  def scanRight[B](z: B)(f: (A, =>B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, p0) => {
+      lazy val p1 = p0
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
 }
 
 case object Empty extends Stream[Nothing]
@@ -140,6 +205,7 @@ object Stream {
 
   // ex 5.10
   def fib(a: Int, b: Int): Stream[Int] = Stream.cons(a, fib(b, a + b))
+
   def fibs: Stream[Int] = fib(0, 1)
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
@@ -150,10 +216,20 @@ object Stream {
 
   // ex 5.12
 
+  def ones1: Stream[Int] = unfold(1)(_ => Some((1, 1)))
+
+  def constant1[A](a: A): Stream[A] = unfold(a)(_ => Some((a, a)))
+
+  def from1(n: Int): Stream[Int] = unfold(n)(s => Some((s, s + 1)))
+
+  def fibs1: Stream[Int] = unfold((0, 1)) {
+    case (a, b) => Some(a -> (b, a + b))
+  }
+
 
   def main(args: Array[String]): Unit = {
     println("take")
-    Stream(1, 2, 3).take(2).toList.foreach(println)
+    Stream(1, 2, 3).take1(1).toList.foreach(println)
     println("drop")
     Stream(122, 211, 3, 31, 1).drop(2).toList.foreach(println)
     println("takeWhile")
@@ -169,5 +245,16 @@ object Stream {
 
     println("fibs:")
     println(fibs.take(10).toList)
+
+    println("ones1 3")
+    println(ones1.take(3).toList)
+
+    println("from1 3")
+    println(from1(3).take(3).toList)
+    println("fibs1")
+    println(fibs1.take(10).toList)
+
+    println("tails")
+    println(Stream(1,2,3).tails.map(_.toList).toList)
   }
 }
